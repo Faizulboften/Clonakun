9d2d4a6643465e8862c7ef8b2ec0ca549c184a17
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-09-29 22:45:05 +0800

    Create Julong.py

---
+++ Julong.py
@@ -0,0 +1,17 @@
+clear
+sleep 5
+echo"----------------------------"
+echo"      📂CLON AKUN📂         "
+echo"      ✔ FAIZUL  ✔         "
+echo"----------------------------"
+sleep 1
+echo"    📂CLON AKUN LUAR    "
+echo"---------------------   "
+sleep 1
+then
+clear
+figlet -f slant "INSTALL COK"|lolcat
+sleep 1
+https://github.com/faizanwahla/pacman
+cd pacman
+python2 pacman.py
